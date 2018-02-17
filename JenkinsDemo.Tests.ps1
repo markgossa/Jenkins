3c7272e4b3732b5453183af41526202c1b022835
Mark Gossa <mark@gossa.co.uk>
2018-02-17 15:24:56 +0000

    Creates tests

---
+++ JenkinsDemo.Tests.ps1
@@ -0,0 +1,31 @@
+﻿param 
+(
+    [Parameter(Mandatory = $true)]
+    [String]
+    $Password,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $Username,
+    [Parameter(Mandatory = $true)]
+    [String]
+    $Computer
+)
+
+$securePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName, $securePassword
+
+
+Describe "Restart-ComputerService" {
+    It "returns nothing" {
+        Invoke-Command -ComputerName $Computer -Credential $cred -ScriptBlock {
+            if(Get-Item C:\temp\jenkinstest.txt)
+            {
+                return $true
+            }
+            else 
+            {
+                return $false
+            }
+        } | Should Be $true
+    }
+}
